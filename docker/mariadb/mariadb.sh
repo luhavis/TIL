334e1a1e54f50a2c941b8d6de5a574a60782ef3c
Ludev <luhavis.dev@gmail.com>
2020-12-18 01:01:50 +0900

    Update mariadb.sh

--- docker/mariadb/mariadb.sh
+++ docker/mariadb/mariadb.sh
@@ -1,7 +1,7 @@
 docker run -d \
 --name mariadb \
 -p 3306:3306 \
--e MYSQL_ROOT_PASSWORD=dahanit12 \
+-e MYSQL_ROOT_PASSWORD=password \
 -e LC_ALL=C.UTF-8 \
 -v ~/data:/var/lib/mysql \
 mariadb:10.4
